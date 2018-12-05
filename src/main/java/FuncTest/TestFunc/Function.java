d4881c4acbd5382fa0d35ee95b215db7428d3eb9
Ofir <ofiralexi@gmail.com>
2018-12-05 20:52:44 +0200

    good working bot

--- src/main/java/FuncTest/TestFunc/Function.java
+++ src/main/java/FuncTest/TestFunc/Function.java
@@ -3,120 +3,132 @@ package FuncTest.TestFunc;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
 import java.sql.Statement;
 import java.util.*;
 import com.microsoft.azure.functions.annotation.*;
-import com.google.api.services.dialogflow.v2.model.GoogleCloudDialogflowV2IntentMessage;
-import com.google.api.services.dialogflow.v2.model.GoogleCloudDialogflowV2IntentMessageSimpleResponse;
-import com.google.api.services.dialogflow.v2.model.GoogleCloudDialogflowV2IntentMessageSimpleResponses;
-import com.google.api.services.dialogflow.v2.model.GoogleCloudDialogflowV2WebhookResponse;
 import com.microsoft.azure.functions.*;
-
-import org.apache.http.entity.StringEntity;
 import org.json.*;
+import FuncTest.TestFunc.globals;
+import FuncTest.TestFunc.utils;
 
 /**
  * Azure Functions with HTTP Trigger.
  */
+@SuppressWarnings("static-method")
 public class Function {
-	@SuppressWarnings("static-method")
 	@FunctionName("CoolFunc")
-    public HttpResponseMessage run(
-            @HttpTrigger(name = "req", methods = {HttpMethod.GET, HttpMethod.POST}, authLevel = AuthorizationLevel.FUNCTION) HttpRequestMessage<Optional<String>> s,
-            final ExecutionContext c) {
-        c.getLogger().info("=========== GET BUSINESS HOURS BY DAY ITAMARs ===========");
-        //===================================
-        // HANDLE HTTP PARAMETERS
-        JSONObject obj = new JSONObject(s.getBody().get().toString());
-        String bname = "";
-        String day = "";
-        if (obj.has("Business"))
-        	bname = obj.getString("Business");
-        else
-        	return createWebhookResponseContent("Please choose a business to look into\n", s);
-        if(obj.has("DayOfWeek"))
-        	day = obj.getString("DayOfWeek");
-        else
-        	return createWebhookResponseContent("Please specify a day\n", s);
-        
-        //===================================
-        // check day valid
-        String [] days_of_week = new String[]{"Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Today","Tomorrow","Now"};
-        day = day.substring(0, 1).toUpperCase() + day.substring(1).toLowerCase();
-        if(!Arrays.asList(days_of_week).contains(day))
-        	return createWebhookResponseContent("What is this day? I did not understand\n", s);
-        
-        //===================================
-        // handle special days
-        if("Now".equals(day) || "Today".equals(day)) // question regarding this day
-        {
-        	  Calendar cal = Calendar.getInstance();
-        	  cal.setTime(new Date());
-        	  cal.add(Calendar.HOUR_OF_DAY, 2);
-        	  day = fromIntToDay(cal.get(Calendar.DAY_OF_WEEK));
-        	  c.getLogger().info(day);
-        }
-        else
-            if("Tomorrow".equals(day))// question regarding the next day
-            {
-          	  Calendar cal = Calendar.getInstance();
-        	  cal.setTime(new Date());
-        	  cal.add(Calendar.HOUR_OF_DAY, 2);
-        	  cal.add(Calendar.DATE, 1);
-        	  day = fromIntToDay(cal.get(Calendar.DAY_OF_WEEK));
-        	  c.getLogger().info(day);
-            }
-        
-        String url = String.format("jdbc:sqlserver://%s:1433;database=%s;user=%s;password=%s;encrypt=true;"
-            + "hostNameInCertificate=*.database.windows.net;loginTimeout=30;", "technobotserver.database.windows.net", "technobot", "techazure2@technobotserver", "TechnionBot1234");
-        Connection connection = null;
-        StringBuilder jsonResult = new StringBuilder();
-        try {
-            connection = DriverManager.getConnection(url);
-             // Create and execute a SELECT SQL statement.
-            String selectSql = "SELECT BusinessName, " + day + " FROM BUSINESSES WHERE CHARINDEX('" + bname + "', BusinessName) != 0";
-             try (Statement statement = connection.createStatement();
-            		 ResultSet resultSet = statement.executeQuery(selectSql))
-             {
-            	if (resultSet.isBeforeFirst() ) // we have values to read
-	                while (resultSet.next()) 
+	public HttpResponseMessage run(
+			@HttpTrigger(name = "req", methods = { HttpMethod.GET,
+					HttpMethod.POST }, authLevel = AuthorizationLevel.FUNCTION) HttpRequestMessage<Optional<String>> s,
+			final ExecutionContext c) {
+		c.getLogger().info("=========== WEBHOOK INVOKED ===========");
+		JSONObject queryResult = new JSONObject(s.getBody().get().toString()).getJSONObject("queryResult");
+		// ===================================
+		// INVOKE RELEVANT INTENT_HANDLER
+		switch (queryResult.getJSONObject("intent").getString("displayName")) {
+		case globals.BUSINESS_HOUR_BY_DAY_INTENT_NAME:
+			return getHourByDay(queryResult, s, c);
+		case globals.BUSINESS_HOUR_WEEK_INTENT_NAME:
+			return getHourByWeek(queryResult, s, c);
+		}
+		return utils.createWebhookResponseContent("what is this intent?", s);
+
+	}
+
+	private HttpResponseMessage getHourByWeek(JSONObject queryResult, HttpRequestMessage<Optional<String>> s,
+			final ExecutionContext c) {
+		c.getLogger().info("=========== GET HOUR BY WEEK ===========");
+		// ===================================
+		// get query parameters
+		JSONObject parameters = queryResult.getJSONObject("parameters");
+		String bname = "";
+		if (parameters.has("Business"))
+			bname = parameters.getString("Business");
+		else
+			return utils.createWebhookResponseContent(globals.MISSING_BUSINESS_PARAM, s);
+		// ===================================
+		Connection connection = null;
+		StringBuilder jsonResult = new StringBuilder();
+		try {
+			connection = DriverManager.getConnection(globals.CONNECTION_STRING);
+			// Create and execute a SELECT SQL statement.
+			String selectSql = "SELECT Sunday,Monday,Tuesday,Wednesday,Thursday,Friday,Saturday FROM Businesses"
+					+ " WHERE CHARINDEX('" + bname + "',BusinessName) != 0";
+			try (Statement statement = connection.createStatement();
+					ResultSet resultSet = statement.executeQuery(selectSql)) {
+				if (!resultSet.isBeforeFirst())
+					jsonResult.append(globals.NO_BUSINESS_FOUND_ERROR);
+				else {
+					jsonResult.append(bname + " is open on:\n");
+					ResultSetMetaData rsmd = resultSet.getMetaData();
+					int columnsNumber = rsmd.getColumnCount();
+					while (resultSet.next())
+						for (int i = 1; i <= columnsNumber; ++i) {
+							String columnValue = resultSet.getString(i);
+							if (!"N\\A".equals(columnValue)) {
+								if (i > 1)
+									jsonResult.append("\n");
+								jsonResult.append("Between " + columnValue + " on " + rsmd.getColumnName(i) + "s");
+							}
+						}
+				}
+				connection.close();
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		return utils.createWebhookResponseContent(jsonResult.toString(), s);
+	}
+
+	private HttpResponseMessage getHourByDay(JSONObject queryResult, HttpRequestMessage<Optional<String>> s,
+			final ExecutionContext c) {
+		c.getLogger().info("=========== GET BUSINESS HOURS BY DAY ===========");
+		// ===================================
+		// get query parameters
+		JSONObject parameters = queryResult.getJSONObject("parameters");
+		String bname = "", day = "";
+		if (parameters.has("Business"))
+			bname = parameters.getString("Business");
+		else
+			return utils.createWebhookResponseContent(globals.MISSING_BUSINESS_PARAM, s);
+		if (parameters.has("DayOfWeek"))
+			day = parameters.getString("DayOfWeek");
+		else
+			return utils.createWebhookResponseContent(globals.MISSING_DAY_PARAM, s);
+		// ===================================
+		// check day is supported
+		day = utils.dayValidation(day);
+		if ("".equals(day))
+			return utils.createWebhookResponseContent("I'm sorry I don't know what day you mean\n", s);
+		// ===================================
+		Connection connection = null;
+		StringBuilder jsonResult = new StringBuilder();
+		try {
+			connection = DriverManager.getConnection(globals.CONNECTION_STRING);
+			// Create and execute a SELECT SQL statement.
+			String selectSql = "SELECT BusinessName, " + day + " FROM BUSINESSES WHERE CHARINDEX('" + bname
+					+ "', BusinessName) != 0";
+			try (Statement statement = connection.createStatement();
+					ResultSet resultSet = statement.executeQuery(selectSql)) {
+				if (resultSet.isBeforeFirst()) // we have values to read
+					while (resultSet.next())
 						if (!"N\\A".equals(resultSet.getString(2)))
-							jsonResult.append("The "+resultSet.getString(1)+" is open between "+resultSet.getString(2)+" on "+day+"s");
+							jsonResult.append("The " + resultSet.getString(1) + " is open between "
+									+ resultSet.getString(2) + " on " + day + "s");
 						else
-							jsonResult.append(resultSet.getString(1)+" is not open on "+day+"s");
-                else
-                	jsonResult.append("No such business found. If this is an error please contact technionbot1@gmail.com");
-                connection.close();
-            }
-        }
-        catch (Exception e) {
-            e.printStackTrace();
-        }
+							jsonResult.append(resultSet.getString(1) + " is not open on " + day + "s");
+				else
+					jsonResult.append(globals.NO_BUSINESS_FOUND_ERROR);
+				connection.close();
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 
-        return createWebhookResponseContent(jsonResult.toString(), s);
-    }
+		return utils.createWebhookResponseContent(jsonResult.toString(), s);
 
+	}
 
-    private static String fromIntToDay(int i) {
-    	String [] days_of_week = new String[]{"zeroDay","Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"};
-    	return days_of_week[i];
-    }
-    
-    public static HttpResponseMessage createWebhookResponseContent(String resultText, HttpRequestMessage<Optional<String>> s){
-    	String json = new JSONObject().put("fulfillmentText", resultText)
-		.put("fulfillmentMessages",
-				new JSONArray().put(new JSONObject().put("simpleResponses",
-						new JSONObject().put("simpleResponses",
-								new JSONArray().put(new JSONObject().put("displayText", "display text")
-										.put("textToSpeech", "display text"))))))
-		.put("payload",
-				new JSONObject().put("google", new JSONObject().put("expectUserResponse", Boolean.TRUE)))
-		.toString();
-    	
-    	HttpResponseMessage resp = s.createResponseBuilder(HttpStatus.OK)
-				.body(json.getBytes()).header("Content-Type", "application/json; charset=UTF-8").header("Accept", "application/json")
-				.build();
-
-    	return resp;
-    }
 }
